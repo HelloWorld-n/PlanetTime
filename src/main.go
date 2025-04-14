2ecc45bb729dfe12293343b98b548bf7b1f51e64
HelloWorld-n <gajica.b@gmail.com>
2025-04-14 17:56:13 +0200

    show current time via ./run.sh

--- src/main.go
+++ src/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"planetTime/planets"
+	"time"
+)
 
+func main() {
+	earthTime := time.Now().UTC()
+	marsTime := planets.NewMarsTime(&earthTime)
+	fmt.Println(marsTime.Format("rot %R %NM %Dth started %V vinquas %L layers %F fragments ago"))
 }
